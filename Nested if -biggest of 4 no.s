89435d8e8633b3d392e85fa6b230e9fc60dfe476
srisaiparimala <112079480+srisaiparimala@users.noreply.github.com>
2022-11-29 19:50:03 +0530

    Create Nested if -biggest of 4 no.s

---
+++ Nested if -biggest of 4 no.s
@@ -0,0 +1,39 @@
+a=int (input())
+
+b=int(input())
+
+c=int (input())
+
+d=int(input()) 
+
+if a>b:
+
+ if a>c:
+
+  if a>d:
+
+    print('a is big')
+
+if b>a:
+
+ if b>c:
+
+   if b>d: 
+
+     print('b id big')
+
+if c>a:
+
+ if c>b:
+
+  if c>d:
+
+    print('c is big')
+
+if d>a:
+
+ if d>b:
+
+  if d>c:
+
+    print('d is big')
